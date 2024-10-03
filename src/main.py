c5105bfc2d1885b33ee0e7aa32148e1201877b48
jocelyn <jzhu47@go.pasadena.edu>
2024-10-03 11:39:31 -0700

    initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       beansprouts                                                  #
+# 	Created:      10/3/2024, 11:37:33 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
